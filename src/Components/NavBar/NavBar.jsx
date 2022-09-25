d604cdec41ed4e21db837eb36ff0225a8a23212b
Jonathan Guerra <guerra.jonathan04@gmail.com>
2022-09-24 22:02:05 -0300

    Desafio1

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import './NavBar.css'
 export default function NavBar() {
   return (
     <nav className='NavBar'>
-        <li>Inicio</li>
+        <li>Inicio</li> 
         <li>Compra</li>
         <li>Carrito</li>
     </nav>
